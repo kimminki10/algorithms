18b646bdd4d3b977b9a9f999ed6545d161bec1e2
김민기 <mingi@MingBookPro.local>
2019-09-02 14:47:57 +0900

    11659

--- acmicpc/11659/11659.cc
+++ acmicpc/11659/11659.cc
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
+int N, M;
+int arr[100005];
+int sum[100005];
 int main() {
-    int N, M;
     scanf(" %d %d", &N, &M);
-    int arr[100005];
-    for (int i = 0; i < N; i++) {
-        scanf("")
+    for (int i = 1; i <= N; i++) {
+        scanf("%d", &arr[i]);
+        sum[i] = sum[i-1] + arr[i];
+    }
+
+    for (int i = 0; i < M; i++) {
+        int a, b;
+        scanf("%d %d", &a, &b);
+        printf("%d\n", sum[b] - sum[a-1]);
     }
 }
