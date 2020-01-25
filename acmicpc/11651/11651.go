59a3e7014db0c2611ed687b6728aec9abc591ca4
kimminki10 <pangshe10@gmail.com>
2020-01-25 14:21:27 +0900

    11651

---
+++ acmicpc/11651/11651.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+)
+
+var writer = bufio.NewWriter(os.Stdout)
+var reader = bufio.NewReader(os.Stdin)
+
+func printf(f string, a ...interface{}) { fmt.Fprintf(writer, f, a...) }
+func scanf(f string, a ...interface{})  { fmt.Fscanf(reader, f, a...) }
+
+type Point struct {
+	x, y int
+}
+
+type SortBy []Point
+
+func (a SortBy) Len() int      { return len(a) }
+func (a SortBy) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a SortBy) Less(i, j int) bool {
+	if a[i].y == a[j].y {
+		return a[i].x < a[j].x
+	} else {
+		return a[i].y < a[j].y
+	}
+}
+
+func main() {
+	defer writer.Flush()
+	var n int
+	scanf("%d\n", &n)
+
+	arr := make(SortBy, n)
+
+	for i := 0; i < n; i++ {
+		scanf("%d %d\n", &arr[i].x, &arr[i].y)
+	}
+	sort.Sort(arr)
+	for i := 0; i < n; i++ {
+		printf("%d %d\n", arr[i].x, arr[i].y)
+	}
+}
